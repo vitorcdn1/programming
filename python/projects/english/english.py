c36db5f0b8b8b36b57b65ae7f9e48cafa9505cb4
vitorcdn1 <vitorcdn1@gmail.com>
2020-08-07 18:04:11 -0300

    a update in english project

--- python/projects/english/english.py
+++ python/projects/english/english.py
@@ -2,4 +2,11 @@ from tkinter import *
 
 root = Tk()
 
+work_place_label = Label(root, text="Path place")
+work_place_entry = Entry(root)
+
+#Position
+work_place_label.grid(row=0, column=0)
+work_place_entry.grid(row=0, column=1)
+
 root.mainloop()
