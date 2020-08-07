310898915b072ecc28ad3e170376cb4195ccff04
vitorcdn1 <vitorcdn1@gmail.com>
2020-08-07 18:08:57 -0300

    I put a title and a text area to edit the file

--- python/projects/english/english.py
+++ python/projects/english/english.py
@@ -1,12 +1,17 @@
 from tkinter import *
+from os import chdir
 
 root = Tk()
+root.title("English")
 
-work_place_label = Label(root, text="Path place")
+# Widgets
+work_place_label = Label(root, text="Path place: ")
 work_place_entry = Entry(root)
+file_text_area = Text(root, width=50, height=20)
 
 #Position
 work_place_label.grid(row=0, column=0)
 work_place_entry.grid(row=0, column=1)
+file_text_area.grid(row=1, column=0, columnspan=2)
 
 root.mainloop()
