2d8ddcc4ef342e35ce8b8caf9bb911e236fc6207
vitorcdn1 <vitorcdn1@gmail.com>
2020-08-08 17:19:50 -0300

    New Function to the english project

--- python/projects/english/english.py
+++ python/projects/english/english.py
@@ -1,8 +1,28 @@
 from tkinter import *
 from os import chdir
 
+def create_new_window(menssage="Do you want to create a new window",):
+
+	window = Toplevel()
+	window.title("Create a new File")
+
+	text = Label(window, text=menssage)
+	button_confirm = Button(window, text="Confirm")
+	button_cancel = Button(window, text="Cancel", command= window.quit)
+	
+
+	text.grid(row=0, column=0,columnspan=3)
+
+def current():
+	try:
+		arquivo = open(".default", r+)
+
+	except:
+		default_path = "/home/vitor/"
+		arquivo = open(".default.txt", "w")
+
 root = Tk()
-	root.title("English")
+root.title("English")
 
 # Widgets
 work_place_label = Label(root, text="Path place: ")
