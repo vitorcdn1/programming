2dc1fc484f107c52c7b8b7bc93e86ecd8c512496
vitorcdn1 <vitorcdn1@gmail.com>
2020-08-08 16:41:38 -0300

    Update of the site1

--- javascript/site1/index.js
+++ javascript/site1/index.js
@@ -1,3 +1,24 @@
-for (var c = ;c < 10;c++{
-	console.log("hello")
+var rubiks = {type: "gans-356",
+		model: "3x3x3"}
+
+function teste(){
+	
+	var increase = 0
+	var biggest = 0
+
+	for (x in rubiks){
+		
+		if (x.length >= biggest){
+			biggest = x.length
+		}
+
+		increase += 1
+	}
+
+	for (x in rubiks){
+		console.log(x.concat(" ").concat(x.length))
+	}
+
 }
+
+teste()
