024daffcec6faad03c7003faa6b95480cab1c390
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-14 20:43:00 -0300

    Update english.py

--- python/projects/english_final/english.py
+++ python/projects/english_final/english.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 
 root = Tk()
-root.geometry(("500x500"))
+root.geometry(("700x500"))
 root.title("English")
 
 def search():
@@ -10,14 +10,24 @@ def search():
 
 def edit():
 	print("edit()")
+
+#Button
+
 button_exit = Button(root, text = "Exit the program",command = root.quit)
 button_search = Button(root, text = "Search", command = search)
 button_edit = Button(root, text = "Edit", command = edit)
+
+# Input
+text = Text(root, height=20, width=80)
 entry_search = Entry(root)
 
+# Inputs position
 
 entry_search.grid(row=0,column=0,columnspan=2)
-button_exit.grid(row=0,column=2)
+text.grid(row = 2, column = 0 , columnspan = 3)
+# Button position
+
+button_exit.grid(row=3,column=1)
 button_search.grid(row=1,column=0)
 button_edit.grid(row=1,column=1)
 
