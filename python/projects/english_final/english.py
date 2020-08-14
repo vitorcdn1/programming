3ad2622486a217e8a5857c6e46732fdfe9754437
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-14 20:29:07 -0300

    english file is up to date

--- python/projects/english_final/english.py
+++ python/projects/english_final/english.py
@@ -5,8 +5,20 @@ root = Tk()
 root.geometry(("500x500"))
 root.title("English")
 
+def search():
+	print("search()")
 
-button_exit = Button(root, text="Exit the program",command=root.quit)
-button_exit.grid(row=0,column=0)
+def edit():
+	print("edit()")
+button_exit = Button(root, text = "Exit the program",command = root.quit)
+button_search = Button(root, text = "Search", command = search)
+button_edit = Button(root, text = "Edit", command = edit)
+entry_search = Entry(root)
+
+
+entry_search.grid(row=0,column=0,columnspan=2)
+button_exit.grid(row=0,column=2)
+button_search.grid(row=1,column=0)
+button_edit.grid(row=1,column=1)
 
 root.mainloop()
