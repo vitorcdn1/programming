6ed619d7b93c6615df6231e111f12a6bf8505e22
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-14 18:17:46 -0300

    Update site3

--- javascript/site3/index.js
+++ javascript/site3/index.js
@@ -1,9 +1,27 @@
-var num1 = Number(window.prompt("Digite o primeiro número: "))
+function readoption(question="write a number: ",array_list=[]){
 
-window.alert(num1)
-if (num1 == NaN){
-	window.alert("Oi vitor")
+	while (true){
+
+		var response = Number(window.prompt("Write a number: "))
+
+		if (Number.isInteger(response) == true && response in array_list){
+			return response
+		}
+		if (array_list == [] && Number.isinteger(reponse) == true){
+
+			return response
+		}
+		if (response == NaN){
+				
+			window.alert("Please write a integer number !!")
+		}
+
+	}
 }
-else {
-	window.alert("hello")
-}
+
+var list = [
+	"Exit program",
+	"Digitar algum número"
+	]
+
+readoption(list)
