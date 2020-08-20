a5e43f972e9b08c819de609f5f82318992694885
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-20 18:02:23 -0300

    javascript

---
+++ css/site1/index.js
@@ -0,0 +1,5 @@
+function teste(){
+        return "Vitor"
+}
+
+exports.MyTestFunction = teste
--- css/site1/server.js
+++ css/site1/server.js
@@ -1,6 +1,9 @@
 var http = require("http")
+var dt = require("./index")
 
 http.createServer(function (req, res){
-        res.write("hello")
-        res.end
+        res.writeHead(200, {"Content-Type": "text/html"})
+        res.end(req.url)
 }).listen(8080)
+
+console.log("running")
