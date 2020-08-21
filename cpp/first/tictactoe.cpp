3c245ef4c90065ab357de904e817b263550868d6
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-20 22:01:28 -0300

    jogo da velha

---
+++ cpp/first/tictactoe.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	
+
+	for (int x = 0;c < 9;x++){
+
+		int player1_position, player2_position;
+
+		cout << "Player 1 = x: ";
+		cin >> player1_position;
+		cout << "Player 2 = 0: ";
+		cin >> player1_position;
+	}
+}
