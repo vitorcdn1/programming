67482fec14b1a388c3d42d9db81ab8c49dc99594
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-20 23:55:14 -0300

    cpp tictactoe

--- cpp/first/tictactoe.cpp
+++ cpp/first/tictactoe.cpp
@@ -2,17 +2,27 @@
 
 using namespace std;
 
+int check(){
+	
+	int tictac[9] = {}
+
+	for (int c = 0;c < 9;c++){
+
+		}
+	}
+}
+
 int main(){
 
-	
-
-	for (int x = 0;c < 9;x++){
+	for (int x = 0;x < 9;x++){
 
 		int player1_position, player2_position;
 
 		cout << "Player 1 = x: ";
 		cin >> player1_position;
-		cout << "Player 2 = 0: ";
+		cout << "Player 2 = o: ";
 		cin >> player1_position;
+
+		
 	}
 }
