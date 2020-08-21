dcc8683fdc0bd26bba1e9cfc2d1b9e0bfc5e76fb
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-21 01:24:44 -0300

    draw function to the program tic tac toe

--- cpp/first/tictactoe.cpp
+++ cpp/first/tictactoe.cpp
@@ -5,18 +5,20 @@ using namespace std;
 
 string draw(string position){
 
-	cout <<   position[0] << " | " << position[1] << " | " << position[2]  << endl;
+	cout << position[0] << " | " << position[1] << " | " << position[2] << endl;
 	cout << "__|___|__ " << endl;
- 	cout << "4 | 5 | 6 " << endl;
+ 	cout << position[3] << " | " << position[4] << " | " << position[5] << endl;
  	cout << "__|___|__ " << endl;
- 	cout << "7 | 8 | 9 " << endl;
+ 	cout << position[6] << " | " << position[7] << " | " << position[8] << endl;
 
 	return 0;
 }
 
 int main(){
 
-	string teste[5] = {"x","o","x"};
+	string teste = {'x','o','x',
+			'o','x','o',
+			'x','o','x'};
 
 	draw(teste);
 	return 0;
