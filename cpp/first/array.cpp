89e414f95408ca391c42c799c9fe0629ad4264d3
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-20 22:00:25 -0300

    jogo da velha

---
+++ cpp/first/array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+	int foo[2] = {50, 80};
+	string teste = "vitor";
+
+	for (int c = 0;c < 10;c++){
+		
+		cout << foo[c] << endl;
+	}
+
+	return 0;
+}
