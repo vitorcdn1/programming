f2e9047d2aeb17227dcc562c6351f6c8814a3119
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-25 20:23:53 -0300

    create_word.py update

--- python/projects/create_word/create_word.py
+++ python/projects/create_word/create_word.py
@@ -1,6 +1,6 @@
 from random import randint, choice
+from termcolor import colored
 from os import system
-
 
 
 alphabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -8,19 +8,33 @@ alphabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def word(num=True,lower=True,upper=True):
 
 	if num == False and lower == False and upper == False:
-		print("ERROR !!!")
-		print("All of the are false")
+		print(colored("ERROR !!! All of the are false", "red"))
 
 	return_list = list()
 
 	
+def ReadYesNo(question):
+
+	while True:
+
+		result = input(question)
+		if result == "yes":
+			return True
+		elif result == "no":
+			return False
+		else:
+			error_menssage = colored("Error do you type (yes/no)", 'red')
+			print(error_menssage)
 
 while True:
 	
 	system("clear")
-	print("Write True or False to generate the word")
-	num1 = bool(input("Do you want numbers: "))
-	num2 = bool(input("Do you want uppercase: "))
-	num3 = bool(input("Do you want lowercase: "))
-	word(num1, num2, num3)
+	print(f"{'-'*10} Create Random Words  {'-'*10}")
+	print("Write (yes/no) to generate the word")
+
+	lower = ReadYesNo("Do you want LowerCase in your password: ")
+	upper = ReadYesNo("Do you want UpperCase in your password: ")
+	numbers = ReadYesNo("Do you want numbers in your password: ")
+
+	word(lower, upper, numbers)
 	input("press return to generate a new word...")
