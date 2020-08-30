d534fba08c31651e447ee1ee831c79d2ee3842da
vitorcdn1 <vitorcdn11@gmail.com>
2020-08-30 01:09:11 -0300

    tabuada program in python

---
+++ python/projects/tabuada/tabuada.py
@@ -0,0 +1,44 @@
+import termcolor
+
+class ReadOption:
+	def __init__(self, option):
+		self.option = option
+
+	def PrintOption(self):
+		print("="*50)
+		biggest = 0
+		for c in range(0,len(self.option)):
+			print(f"{c} - {self.option[c]}")
+		print("="*50)
+
+	def ReadOption(self, question):
+
+		num_options = []
+
+		if len(self.option) >= 1:
+			for c in range(0,len(self.option)):
+				num_options.append(str(c))
+
+			while True:
+				responce = input(question)
+
+				if responce.isnumeric() == True:
+
+					if responce in num_options:
+						return int(responce)
+					else:
+						print(termcolor.colored("Error Please type a valid option !!!!", "red"))
+						break
+
+				else:
+					print(termcolor.colored("Error Please Type a number !!!!", "red"))
+					break
+					
+
+
+while True:
+	question = ReadOption(["Sair do programa","Learn"])
+	question.PrintOption() #Print the options
+	responce = question.ReadOption("Type a option: ")
+	if responce == 0:
+		break
